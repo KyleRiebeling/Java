93eb63e3ec0abe33d6b4b5383a70674c129e3237
KyleRiebeling <125304972+KyleRiebeling@users.noreply.github.com>
2023-04-21 16:10:41 -0700

    Added comments to the top of Poker.java

--- Poker/Poker.java
+++ Poker/Poker.java
@@ -1,6 +1,12 @@
 package Poker;
-
-
+/*Programmer Name: Kyle Riebeling
+ * Assignment Start: 4:05 PM 4/21/2023
+ * Assignment Completion: 7:16 PM 4/21/2023
+ * Total Hours of Assignment: ~ 3 hours
+ * Comments: This was a fun project. Its also very useful because I happen to have a 
+ *           "card game" assignment due in my c++ class, and we were given permission to
+ *           simply modify a game project from another class.
+ */
 
 public class Poker {
     public static void main(String[] args) {
